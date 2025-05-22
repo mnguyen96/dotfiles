7936d2f48e30478d15eb44711e5007f313f189d2
Michael Nguyen <michaelalexnguyen@gmail.com>
2025-05-22 11:05:25 -0400

    update

--- .config/nvim/lua/plugins/hardtime.lua
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -1,18 +1,24 @@
+-- return {
+--   "m4xshen/hardtime.nvim",
+--   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+--   opts = {
+--     enabled = false,
+--   },
+--   command = "Hardtime",
+--   event = "BufEnter",
+--   keys = {
+--     { "n", "j", "<cmd>Hardtime<CR>", desc = "Hardtime" },
+--     { "n", "k", "<cmd>Hardtime<CR>", desc = "Hardtime" },
+--     -- { "j", 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"' },
+--     -- { "k", 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"' },
+--     { "n", "gj", "<cmd>Hardtime<CR>", desc = "Hardtime" },
+--     { "n", "gk", "<cmd>Hardtime<CR>", desc = "Hardtime" },
+--     { "n", "n", false },
+--   },
+-- }
 return {
   "m4xshen/hardtime.nvim",
-  dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-  opts = {
-    enabled = false,
-  },
-  command = "Hardtime",
-  event = "BufEnter",
-  keys = {
-    { "n", "j", "<cmd>Hardtime<CR>", desc = "Hardtime" },
-    { "n", "k", "<cmd>Hardtime<CR>", desc = "Hardtime" },
-    -- { "j", 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"' },
-    -- { "k", 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"' },
-    { "n", "gj", "<cmd>Hardtime<CR>", desc = "Hardtime" },
-    { "n", "gk", "<cmd>Hardtime<CR>", desc = "Hardtime" },
-    { "n", "n", false },
-  },
+  lazy = false,
+  dependencies = { "MunifTanjim/nui.nvim" },
+  opts = {},
 }
